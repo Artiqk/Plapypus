d72d551005e6cc10af0dcee643c9099293580f11
Antoine Abonneau <antoineabonneau94@gmail.com>
2024-06-24 23:17:44 +0200

    refactored functions to class

--- password_manager/core.py
+++ password_manager/core.py
@@ -1,35 +1,55 @@
 from termcolor import colored
-from password_manager.utils import load_json_data, save_json_data, credentials_already_exist
+from password_manager.utils import *
+from password_manager.exceptions import *
 
 
-def add_website(filename, website, username, password):
-    data = load_json_data(filename)
+class CredentialManager:
     
-    if website in data and credentials_already_exist(data, website, username):
-        print(colored(f'`{username}` for {website} already exists.', 'red'))
-        return
-    
-    if website not in data:
-        data[website] = []
-    
-    data[website].append({'username': username, 'password': password})
-    
-    if save_json_data(filename, data):
-        print(colored(f'`{username}` for {website} has successfully been written.', 'green'))
+    def __init__(self, filename):
+        self.filename = filename
+
+    def add(self, website, username, password):
+        data = load_json_data(self.filename)
+        
+        if website in data and credentials_already_exist(data, website, username):
+            raise CredentialAlreadyExistsError(username, website)
+        
+        if website not in data:
+            data[website] = []
+        
+        data[website].append({'username': username, 'password': password})
+        
+        return save_json_data(self.filename, data)
 
 
-def add_websites(filename, new_website_data_list):
-    for new_website_data in new_website_data_list:
-        add_website(filename, **new_website_data)
+    def add_multiple(self, new_website_data_list):
+        for new_website_data in new_website_data_list:
+            self.add_credentials_for_website(**new_website_data)
         
 
-def list_websites(filename):
-    data = load_json_data(filename)
-    
-    for website, credentials in data.items():
-        print(colored(website, 'blue'))
-        for credential in credentials:
-            print('\t=>', end=' ')
-            print(colored(f'{credential["username"]}', 'green'), end=':')
-            print(colored(f'{credential["password"]}', 'red'))
-        print('============================')
+    def list(self):
+        data = load_json_data(self.filename)
+        
+        for website, credentials in data.items():
+            print(colored(website, 'blue'))
+            for credential in credentials:
+                print('\t=>', end=' ')
+                print(colored(f'{credential["username"]}', 'green'), end=':')
+                print(colored(f'{credential["password"]}', 'red'))
+            print()
+        
+
+    def remove(self, website, username):
+        data = load_json_data(self.filename)
+        
+        if website in data:
+            index_to_remove = find_index_by_key_value(data[website], 'username', username)
+            
+            if index_to_remove != -1:
+                data[website].pop(index_to_remove)
+            else:
+                raise CredentialNotFoundError(username, website)
+        else:
+            raise WebsiteNotFoundError(website)
+        
+        return save_json_data(self.filename, data)
