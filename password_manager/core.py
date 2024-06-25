5b316954521f1df1366d3169bc223c747796680f
Antoine Abonneau <antoineabonneau94@gmail.com>
2024-06-25 05:19:07 +0200

    added security layer

--- password_manager/core.py
+++ password_manager/core.py
@@ -7,9 +7,19 @@ class CredentialManager:
     
     def __init__(self, filename: str) -> None:
         self.filename = filename
+        
+        
+    def load_data(self, master_password: str) -> dict:
+        return load_encrypted_json_data(self.filename, master_password)
+    
+    
+    def save_data(self, data: dict, master_password: str) -> None:
+        save_encrypted_json_data(self.filename, data, master_password)
+        del master_password
 
-    def add(self, website: str, username: str, password: str) -> bool:
-        data = load_json_data(self.filename)
+
+    def add(self, website: str, username: str, password: str, master_password: str) -> bool:
+        data = self.load_data(master_password)
         
         if website in data and credentials_already_exist(data, website, username):
             raise CredentialAlreadyExistsError(username, website)
@@ -19,29 +29,34 @@ class CredentialManager:
         
         data[website].append({'username': username, 'password': password})
         
-        return save_json_data(self.filename, data)
+        self.save_data(data, master_password)
 
 
-    def add_multiple(self, new_website_data_list: list) -> None:
+    def add_multiple(self, new_website_data_list: list, master_password: str) -> None:
         for new_website_data in new_website_data_list:
-            self.add_credentials_for_website(**new_website_data)
+            self.add(**new_website_data, master_password=master_password)
         
 
-    def list(self) -> None:
-        data = load_json_data(self.filename)
+    def list(self, master_password: str) -> None:
+        data = self.load_data(master_password)
+        
+        del master_password
         
         for website in sorted(data.keys()):
             credentials = data[website]
             print(colored(website, 'blue'))
-            for credential in credentials:
+            
+            sorted_credentials = sorted(credentials, key=lambda x: x['username'])
+            
+            for credential in sorted_credentials:
                 print('\t=>', end=' ')
                 print(colored(f'{credential["username"]}', 'green'), end=':')
                 print(colored(f'{credential["password"]}', 'red'))
             print()
 
     
-    def update(self, website: str, username: str, new_password: str) -> bool:
-        data = load_json_data(self.filename)
+    def update(self, website: str, username: str, new_password: str, master_password: str) -> None:
+        data = self.load_data(master_password)
         
         if website not in data:
             raise WebsiteNotFoundError(website)
@@ -53,11 +68,11 @@ class CredentialManager:
         
         data[website][index_to_update]['password'] = new_password
         
-        return save_json_data(self.filename, data)
+        self.save_data(data, master_password)
 
 
-    def remove(self, website: str, username: str) -> bool:
-        data = load_json_data(self.filename)
+    def remove(self, website: str, username: str, master_password: str) -> None:
+        data = self.load_data(master_password)
         
         if website not in data:
             raise WebsiteNotFoundError(website)
@@ -72,4 +87,4 @@ class CredentialManager:
         if not data[website]:
             del data[website]
         
-        return save_json_data(self.filename, data)
+        self.save_data(data, master_password)
--- password_manager/utils.py
+++ password_manager/utils.py
@@ -1,24 +1,62 @@
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.fernet import Fernet
 import json
+import base64
+import os
 
 
-def load_json_data(filename: str) -> dict:
-    try:
-        with open(filename, 'r') as file:
-            existing_data = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        existing_data = {}
+def derive_key(password: str, salt: bytes) -> bytes:
+    kdf = Scrypt(
+        salt=salt,
+        length=32,
+        n=2**14,
+        r=8,
+        p=1,
+        backend=default_backend()
+    )
     
-    return existing_data
-
-
-def save_json_data(filename: str, data: dict) -> bool:
-    try:
-        with open(filename, 'w') as file:
-            json.dump(data, file)
-    except IOError as e:
-        return False
+    key = kdf.derive(password.encode())
     
-    return True
+    return base64.urlsafe_b64encode(key)
+
+
+def load_encrypted_json_data(filename: str, password: str) -> dict:
+    try:
+        with open(filename, 'rb') as file:
+            encrypted_data = file.read()
+        
+        salt = encrypted_data[:16]
+        encrypted_data = encrypted_data[16:]
+        
+        key = derive_key(password, salt)
+        
+        fernet = Fernet(key)
+        
+        decrypted_data = fernet.decrypt(encrypted_data).decode()
+        
+        return json.loads(decrypted_data)
+    
+    except FileNotFoundError:
+        return {}
+    
+    except Exception as e:
+        raise RuntimeError('Failed to decrypt data') from e
+    
+    
+def save_encrypted_json_data(filename: str, data: dict, password: str) -> None:
+    salt = os.urandom(16)
+    key = derive_key(password, salt)
+    fernet = Fernet(key)
+    encrypted_data = fernet.encrypt(json.dumps(data).encode())
+    
+    fd = os.open(filename, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o600)
+    
+    with os.fdopen(fd, 'wb') as file:
+        file.write(salt)
+        file.write(encrypted_data)
 
 
 def credentials_already_exist(website_data: dict, website: str, username: str) -> bool:
